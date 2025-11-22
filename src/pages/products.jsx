5a547161048242d07a9d74d63cc870c5aace4aa4
rahmatismaill <rahmatismaill173@gmail.com>
2025-11-22 21:12:47 +0800

    membuat card products dan menambah folder img

---
+++ src/pages/products.jsx
@@ -0,0 +1,20 @@
+export default function Products() {
+  return (
+    <>
+      <div className="navProducts">Rahmat Ismail</div>
+      <div className="productsContainer">
+        <div className="products">
+          <div className="cards">
+            <img src="/img/foto1.jpg" alt="" />
+          </div>
+          <h3>Keyboard</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo,
+            voluptas voluptatum consectetur repellendus inventore laboriosam?
+          </p>
+          <button>Buy Now</button>
+        </div>
+      </div>
+    </>
+  );
+}
